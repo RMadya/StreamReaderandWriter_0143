7b4b0088d892fe0bc0e9d0dc76d44ecd104ac362
RMadya <kereyoben75@gmail.com>
2024-06-03 10:30:41 +0700

    penangkap objek exception

--- ExceptionStandart/ExceptionStandart.cpp
+++ ExceptionStandart/ExceptionStandart.cpp
@@ -10,7 +10,13 @@ int main() {
 	try {
 		array<int, 3> data = { 1,2,3 };
 		//pesan array integer 3 element
-		cout << data.at(2) << endl;
+		cout << data.at(5) << endl;
 		//memanggil array element ke 5 {menyesuaikan jumlah array yang ada tdk bsa lebih dari jumlah yg ada}
 	}
+	catch (exception& e) {
+		//penangkap menggunakan objek exception
+		cout << e.what() << endl;
+		/*akan dieksekusi karna array data hanya memiliki 3 elemen*/
+	}
+	
 }
