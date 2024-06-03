d214dfcc46d3f9de9e6b6e1dbac552e24ede6fcf
RMadya <kereyoben75@gmail.com>
2024-06-03 10:10:41 +0700

    membuka file dan menunjuk ke sebuah nama file

---
+++ ioFileWichPach.cpp/ioFileWichPach.cpp.cpp
@@ -0,0 +1,17 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string baris;
+	string NamaFile;
+
+	cout << "Masukan nama file : ";
+	cin >> NamaFile;
+
+	//mambuka file dalam mode menulis
+	ofstream outfile;
+	//menunjuk ke sebuah nama file
+	outfile.open(NamaFile + ".txt", ios::out);
+}
